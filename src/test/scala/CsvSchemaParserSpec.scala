667e2caed30d74763796350043b3f26652c5c4d4
Ahmedbjebara <ahmedbenj94@gmail.com>
2019-11-14 11:20:47 +0100

    test src

---
+++ src/test/scala/CsvSchemaParserSpec.scala
@@ -0,0 +1,44 @@
+import org.apache.spark.sql.types._
+import org.scalatest.{FlatSpec, GivenWhenThen, Matchers}
+
+
+class ProcessDataFileSpec extends FlatSpec with GivenWhenThen with Matchers{
+
+  behavior of "CsvSchemaParser"
+
+  val ReferenceSchema = StructType(
+    StructField("_c0", IntegerType, false) ::
+      StructField("carat", StringType, false) ::
+      StructField("cut", StringType, false) ::
+      StructField("color", StringType, false) ::
+      StructField("clarity", StringType, false) ::
+      StructField("depth", DoubleType, false) ::
+      StructField("table", StringType, false) ::
+      StructField("price", StringType, false) ::
+      StructField("x", DoubleType, false) ::
+      StructField("y", DoubleType, false) ::
+      StructField("z", DoubleType, false) :: Nil)
+
+  val destinationDataType=IntegerType
+
+  it should "parse csv file to schema" in {
+    Given ("an absolute path of a csv file")
+    val path = "C:/Users/dell/IdeaProjects/Realtimecheksum/projet/schema/schemaout.csv"
+    When ("parseCsvFileToSchema is invoked")
+    val resultatSchema =  ProcessDataFiles.csvFileToStructType(path)
+    Then (" a schema should be returned ")
+    resultatSchema should equal (ReferenceSchema)
+
+  }
+
+  it should "replace type with datatype" in {
+    Given ("a string")
+    val sourceType= "Integer"
+    When ("replaceTypeToDatatype is invoked")
+    val resultatDataType =  ProcessDataFiles.replaceTypeWithDatatype(sourceType)
+    Then (" a datatype should be returned ")
+    resultatDataType should equal (destinationDataType)
+
+  }
+
+}
